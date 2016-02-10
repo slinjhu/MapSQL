43d4c452c3b47e5e36d6c1886ef162dfa8b6d9b3
Sen Lin <slin@jhu.edu>
2016-02-09 23:42:48 -0500

    Added python script to initialize database

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # MapSQL
 Run sql and show the result on US map (for Copewell Project)
+
+## To use
+- Put all measure csv files into `data` folder. All measures should be scaled but not adjusted for direction. 
+- s
---
+++ initDB.py
@@ -0,0 +1,22 @@
+# read measures data from the data folder and generate the database table.
+import sqlite3
+import pandas
+import glob
+import os
+from natsort import natsorted
+
+conn = sqlite3.connect('copewell.db')
+c = conn.cursor()
+c.execute('DROP TABLE IF EXISTS measure')
+
+data = pandas.read_csv('county_48ALHI.csv', index_col=0)
+
+for file in natsorted(glob.glob('data/*.csv')):
+    id = 'm' + os.path.splitext(os.path.basename(file))[0]
+    df = pandas.read_csv(file, index_col=0)
+    df.columns = [id]
+    data = data.join(df, how='left')
+
+data.to_sql('measure', conn)
+
+conn.close()
